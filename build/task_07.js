8d0750d6f7f9c455ceeb23ebe3ef0f1e31fa3d0c
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-25 12:18:39 -0300

    feat: add new feature - task_07.

---
+++ build/task_07.js
@@ -0,0 +1,12 @@
+"use strict";
+let name_task07 = "Rilix Ltda";
+let adress_task07 = "Rua Jose Belegante, 85 - Centro, Santa Terezinha - SC, 89.199-000";
+let cep_task07 = 89199000;
+let status_task07 = "Ativo";
+let company = {
+    name_task07,
+    adress_task07,
+    cep_task07,
+    status_task07
+};
+console.log(company.name_task07);
