d01f4a04fa944b3ea0fc879414b40e8ecdc37959
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-25 11:57:57 -0300

    feat: add new feature - task_06.

---
+++ build/task_06.js
@@ -0,0 +1,4 @@
+"use strict";
+let student_task06 = [" ", 0];
+student_task06.push("Yasmin Sousa", 27);
+console.log(student_task06);
---
+++ src/task_06.ts
@@ -0,0 +1,5 @@
+// array - tuples
+let student_task06:[string, number] = [" ", 0];
+
+student_task06.push("Yasmin Sousa", 27);
+console.log(student_task06)
