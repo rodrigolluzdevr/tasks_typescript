90fe5975925ee45018b3a3f0bf09f6307fb7d4a8
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-24 21:59:49 -0300

    feat: add new feature - task_05.

---
+++ build/task_05.js
@@ -0,0 +1,9 @@
+"use strict";
+let number_task05 = [20, 30, 40];
+console.log(number_task05);
+number_task05.push(50);
+number_task05.unshift(10);
+console.log(number_task05);
+number_task05.pop();
+number_task05.shift();
+console.log(number_task05);
