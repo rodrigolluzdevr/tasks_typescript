3d57fbc8c2b95b7961babcc8e87525c09610115d
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-28 19:38:36 -0300

    feat: new feature - Disruption.

---
+++ build/task-17.js
@@ -0,0 +1,4 @@
+"use strict";
+let user = ["Rodrigo Sousa Luz", "Brazilian", "developer"];
+let [name_, nation, work] = user;
+console.log(`Name: ${name_}, Nation: ${nation}, Work: ${work}.`);
---
+++ src/task-17.ts
@@ -0,0 +1,6 @@
+// Disruption
+let user: string[] = ["Rodrigo Sousa Luz", "Brazilian", "developer"]
+
+let [name_, nation, work] = user
+
+console.log(`Name: ${name_}, Nation: ${nation}, Work: ${work}.`)
