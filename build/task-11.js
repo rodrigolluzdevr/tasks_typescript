4a2c91d53be3292ec192a2e1e2c9f61e08389daf
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-27 21:59:33 -0300

    feat: add new feature - arrow functions task_10

---
+++ build/task-11.js
@@ -0,0 +1,9 @@
+"use strict";
+const logMagicTest = () => {
+    console.log("The test arrow function");
+};
+const testMagicLog = (txt) => {
+    console.log(txt);
+};
+logMagicTest();
+testMagicLog("The teste parameter in the arrow function");
---
+++ src/task-11.ts
@@ -0,0 +1,12 @@
+//Arrow Function
+const logMagicTest = (): void => {
+    console.log("The test arrow function");
+};
+
+const testMagicLog = (txt: string): void => {
+    console.log(txt);
+};
+
+//Exec
+logMagicTest();
+testMagicLog("The teste parameter in the arrow function");
