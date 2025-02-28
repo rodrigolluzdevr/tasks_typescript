7748fd8365203ffc99e26272e2d9088438052f43
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-27 21:40:05 -0300

    refactor: refactor organization in code

--- build/task-10.js
+++ build/task-10.js
@@ -2,21 +2,23 @@
 function division(n1, n2) {
     return n1 / n2;
 }
-function login(user, password) {
-    console.log(`user: ${user}`);
-    console.log(`password: ${password}`);
-}
-function newUser(user, pass, name) {
+function newUser(user, password, name) {
     const dados = {
         user,
-        pass,
+        password,
         name,
     };
     console.log(dados);
 }
-const result01 = division(10, 2);
-const result02 = division(100, 10).toString();
-console.log(result01);
-console.log(result02);
-login("rodrigo", "Senha@123");
+function login(user, password) {
+    console.log(`user: ${user}`);
+    console.log(`password: ${password}`);
+}
+const result_01 = division(10, 2);
+const result_02 = division(100, 10).toString();
+console.log(typeof (result_01));
+console.log(result_01);
+console.log(typeof (result_02));
+console.log(result_02);
 newUser("yasmin", "Senha@123");
+login("rodrigo", "Senha@123");
--- src/task-10.ts
+++ src/task-10.ts
@@ -3,27 +3,29 @@ function division(n1: number, n2: number): number {
     return n1 / n2;
 }
 
-function login(user: string, password: string): void {
-    console.log(`user: ${user}`);
-    console.log(`password: ${password}`);
-}
-
-function newUser(user: string, pass: string, name?: string): void {
+function newUser(user: string, password: string, name?: string): void {
     const dados = {
         user,
-        pass,
+        password,
         name,
     };
     console.log(dados);
 }
 
+function login(user: string, password: string): void {
+    console.log(`user: ${user}`);
+    console.log(`password: ${password}`);
+}
+
 // Exec
-const result01: number = division(10, 2);
-const result02: string = division(100, 10).toString();
+const result_01: number = division(10, 2);
+const result_02: string = division(100, 10).toString();
 
-console.log(result01);
-console.log(result02);
-
-login("rodrigo", "Senha@123");
+console.log(typeof(result_01))
+console.log(result_01);
+console.log(typeof(result_02))
+console.log(result_02);
 
 newUser("yasmin", "Senha@123");
+
+login("rodrigo", "Senha@123");
